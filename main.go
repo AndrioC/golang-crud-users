3fd61bce6c3aa9964bf3b1029162f257a245f347
Andrio <andrio.rodrigo.silva@hotmail.com>
2023-08-13 15:49:01 -0300

    add loggers

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/andrioc/golang-crud-users/src/config/logger"
 	"github.com/andrioc/golang-crud-users/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("starting app")
 	err := godotenv.Load()
 
 	if err != nil {
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,15 +3,19 @@ package controller
 import (
 	"fmt"
 
+	"github.com/andrioc/golang-crud-users/src/config/logger"
 	"github.com/andrioc/golang-crud-users/src/config/validation"
 	"github.com/andrioc/golang-crud-users/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("init CreateUser controller", zap.String("journey", "createUser"))
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("error trying to validate user info", err, zap.String("journey", "createUser"))
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
