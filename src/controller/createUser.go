893fb40f1fc8a4361a893f76271de26a4392212a
Andrio <andrio.rodrigo.silva@hotmail.com>
2023-08-13 14:37:08 -0300

    add user request and response validation

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,21 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/andrioc/golang-crud-users/src/config/rest_err"
+	"github.com/andrioc/golang-crud-users/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("there are some incorrect fields, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
